df4142fb0f5ae26f7f02c4e33dde295a359382ed
anatolii <anatoliise@mail.ru>
2022-03-05 13:44:23 +0300

    task1

---
+++ задача1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите первое число ");
+string strA = Console.ReadLine();
+int A = int.Parse(strA);
+
+Console.WriteLine("введите второе число ");
+string strB = Console.ReadLine();
+int B = int.Parse(strB);
+
+if(A > B) Console.WriteLine("максимальное число " + A);else Console.WriteLine("максимальное число " + B);
+if(A < B) Console.WriteLine("минимальное число " + A);else Console.WriteLine("минимальное число " + B);
