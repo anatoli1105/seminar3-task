6e9df6edf9d410202e666cf75ccf814eb141bbb0
anatolii <anatoliise@mail.ru>
2022-03-08 16:53:58 +0300

    task1//

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите числа");
+string strN = Console.ReadLine();
+int N =int.Parse(strN);
+if(N%100%10*10+N%100/10 == N/1000)Console.WriteLine("палиндром");
+else Console.WriteLine( "не палиндром");
