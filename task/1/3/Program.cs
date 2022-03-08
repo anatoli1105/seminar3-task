948b3ed9643c2e8cd3ab464c68ad69e7d5eac239
anatolii <anatoliise@mail.ru>
2022-03-08 16:57:42 +0300

    task

---
+++ task/1/3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите числа");
+string strN = Console.ReadLine();
+int N =int.Parse(strN);
+//int [] arrey = new int [5];
+int A = N/10000;
+int b = N/1000%10;
+int c = N/100%10;
+int e = N/10%10;
+int f = N%10;
+int [] array = { A, b, c, e, f};
+if(array[0] == array[4] || array[1] == array[3])
+Console.WriteLine("палиндром");
+else Console.WriteLine("не палиндром");
