324706016be80bcd9b95250bfbb5c580ac68d5e5
anatolii <anatoliise@mail.ru>
2022-03-08 16:48:31 +0300

    task1

--- задача1/Program.cs
+++ задача1/Program.cs
@@ -1,10 +1,7 @@
-﻿Console.WriteLine("введите первое число ");
-string strA = Console.ReadLine();
-int A = int.Parse(strA);
-
-Console.WriteLine("введите второе число ");
-string strB = Console.ReadLine();
-int B = int.Parse(strB);
-
-if(A > B) Console.WriteLine("максимальное число " + A);else Console.WriteLine("максимальное число " + B);
-if(A < B) Console.WriteLine("минимальное число " + A);else Console.WriteLine("минимальное число " + B);
+﻿Console.WriteLine("введите числа");
+string N = Console.ReadLine();
+int array = N.Length;
+int count = 0;
+if(N[count] == N[count + 4] || N[count + 1] == N[count + 3])
+Console.WriteLine("палиндром");
+else Console.WriteLine("не палиндром");
